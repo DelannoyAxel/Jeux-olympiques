1c9da9f25ad7c47c530f1ecfed88db9d5b544538
DelannoyAxel <delannoy.axel2@gmail.com>
2024-07-01 16:43:06 +0200

    footer

--- template.php
+++ template.php
@@ -10,7 +10,8 @@ if (session_status() == PHP_SESSION_NONE) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/style.css">
+    <link rel="stylesheet" href="../public/css/template.css">
+    <link rel="stylesheet" href="../public/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
@@ -21,44 +22,44 @@ if (session_status() == PHP_SESSION_NONE) {
 
 <body>
 <header>
-    <!-- Navbar PC -->
-    <nav class="navPc">
-        <div class="GroupeLeft">
-            <a href="index.php">ACCUEIL</a>
-            <a href="classement.php">CLASSEMENT</a>
-        </div>
+        <!-- Navbar PC -->
+        <nav class="navPc">
+            <div class="GroupeLeft">
+                <a href="index.php">ACCUEIL</a>
+                <a href="classement.php">CLASSEMENT</a>
+            </div>
 
-        <img class="logoJo" src="./public/images/LOGO JO 1.svg" alt="Logo des jeux olympiques">
+            <img class="logoJo" src="./public/images/LOGO JO 1.svg" alt="Logo des jeux olympiques">
 
-        <div class="GroupeRight">
-            <a href="#">INFORMATIONS</a>
-            <a href="login.php">CONNEXION</a>
-            <?php if (isset($_SESSION['idUtilisateur'])) : ?>
-                <a class="navProfil" href="profil.php">MON PROFIL</a>
-            <?php endif; ?>
-        </div>
-    </nav>
+            <div class="GroupeRight">
+                <a href="#">INFORMATIONS</a>
+                <a href="login.php">CONNEXION</a>
+                <?php if (isset($_SESSION['idUtilisateur'])) : ?>
+                    <a class="navProfil" href="profil.php">MON PROFIL</a>
+                <?php endif; ?>
+            </div>
+        </nav>
 
-    <!-- Navbar mobile -->
-    <nav class="navTel">
-        <div class="burger" id="burger">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+        <!-- Navbar mobile -->
+        <nav class="navTel">
+            <div class="burger" id="burger">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
 
-        <div class="menu" id="menu">
-            <a href="index.php">ACCUEIL</a>
-            <a href="classement.php">CLASSEMENT</a>
-            <a href="#">INFORMATIONS</a>
-        </div>
+            <div class="menu" id="menu">
+                <a href="index.php">ACCUEIL</a>
+                <a href="classement.php">CLASSEMENT</a>
+                <a href="#">INFORMATIONS</a>
+            </div>
 
-        <img class="logoJo" src="./public/images/LOGO JO 1.svg" alt="Logo des jeux olympiques">
+            <img class="logoJo" src="./public/images/LOGO JO 1.svg" alt="Logo des jeux olympiques">
 
-        <div class="profil" id="profil">
-            <i class="fa-solid fa-user" style="color: #ffffff;"></i>
-        </div>
-    </nav>
+            <div class="profil" id="profil">
+                <i class="fa-solid fa-user" style="color: #ffffff;"></i>
+            </div>
+        </nav>
 </header>
 
 
@@ -66,9 +67,19 @@ if (session_status() == PHP_SESSION_NONE) {
     <?= $content ?>
 
     <footer>
-        <div>
-            <p>Axel Delannoy @All right reserved</p>
-            <p>Par Royalties — https://www.paris2024.org/fr/, marque déposée, https://fr.wikipedia.org/w/index.php?curid=11309986</p>
+        <div class="footerColumn">
+            <div class="footerLinks">            
+                <h4>Jeux Olympiques</h4>
+                <a href="index.php">ACCUEIL</a>
+                <a href="classement.php">CLASSEMENT</a>
+                <a href="#">INFORMATIONS</a>
+            </div>
+
+            <div class="royalties">
+                <hr>
+                    <p>Axel Delannoy @All right reserved</p>
+                    <p>Par Royalties — https://www.paris2024.org/fr/</p>
+            </div>            
         </div>
     </footer>
 
